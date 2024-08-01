f8265ebb9066e31a642d87a1de19966396a1694e
Ilef01 <bchinyyilef@gmail.com>
2024-08-02 01:01:12 +0200

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+A type-annotated function that takes a string str1 and a string str2
+as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Takes a string str1 and a string str2 as arguments and returns a
+    concatenated string
+    """
+    return str1 + str2
