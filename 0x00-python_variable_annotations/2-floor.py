32a7f6af5f230f91f6ef4bf999744c6d03f866d1
Ilef01 <bchinyyilef@gmail.com>
2024-08-02 22:00:52 +0200

    fixing the return type-annotation of the function

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -8,7 +8,7 @@ and returns the floor of the float
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """
     Takes a float n as argument and returns the floor of the
     float
