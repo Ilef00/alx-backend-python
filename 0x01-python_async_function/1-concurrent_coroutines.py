d3c00c92e21c838a0c020768c4da8622d2beee06
Ilef01 <bchinyyilef@gmail.com>
2024-08-08 02:31:27 +0200

    adding variable and return value annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,18 +8,21 @@ and returns the list of n delays (in ascending order)
 
 import asyncio
 from heapq import heappush, heappop
+from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Takes 2 int argumentsn and man_delay and returns a list of n
     delays in ascending order
     """
-    delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = []
+    tasks: List[asyncio.task] = [
+        asyncio.create_task(wait_random(max_delay)) for _ in range(n)
+    ]
     for task in asyncio.as_completed(tasks):
-        delay = await task
+        delay: float = await task
         heappush(delays, delay)
     return [heappop(delays) for _ in range(len(delays))]
